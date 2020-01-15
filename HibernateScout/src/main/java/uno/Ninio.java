2f9e25c5f8fdb6783333ff7d53a303caeafe008f
miguel <miguel@miguel-UX310UAK>
2020-01-15 19:05:35 +0100

    aaa

--- HibernateScout/src/main/java/uno/Ninio.java
+++ HibernateScout/src/main/java/uno/Ninio.java
@@ -3,7 +3,7 @@ package uno;
 public class Ninio {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stuba
+		// TODO Auto-generated method stubaaaaa
 		System.out.println("Sup world");
 	}
 
